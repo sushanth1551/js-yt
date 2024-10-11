3a71a3c437e8588786b9f3e476250fafd7cea056
sushanth1551 <sushanth1551@gmail.com>
2024-10-11 17:41:04 +0000

    conandop

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,17 @@
+let score ="sush"
+console.log(typeof score);
+
+let valueInNumber= Number(score)
+console.log(typeof valueInNumber);
+
+let isloggedIn ="sushanth"
+let booleanIsLoggedIn = Boolean(isloggedIn)
+console.log(booleanIsLoggedIn);
+
+let someNumber = 33
+let StringNumber = String (someNumber)
+console.log(StringNumber);
+console.log(typeof StringNumber);
+
+
+
---
+++ 01_basics/04_operations.js
@@ -0,0 +1,23 @@
+let value =3
+let negvalue =-value
+
+console.log(negvalue);
+
+let str1 = "hello"
+let str2 ="  sush"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log("1"+"2");
+console.log((3+9) *5%9);
+
+console.log(+true);
+console.log(+"");
+
+ 
+
+
+
+

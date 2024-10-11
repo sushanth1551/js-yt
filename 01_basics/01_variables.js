584a570420e183db1ec97fef89154917580ca657
sushanth1551 <sushanth1551@gmail.com>
2024-10-11 15:18:18 +0000

    var and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId =123455
+let accountEmail = "sush@gmail.com"
+var accountPassword = "4567"
+accountCity="hyd"
+let accountState;
+
+accountEmail="hchc@gmail.com"
+accountPassword="999999"
+accountCity="tel"
+
+console.table([accountEmail,accountId,accountPassword,accountState,accountCity]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,13 @@
+"use strict";
+
+console.log(3
+    +
+    4)
+console.log("sush")
+
+let name = "sush"
+let age = 18
+let isloggedIn = true
+
+console.log(typeof "sush")
+console.log(typeof age);
